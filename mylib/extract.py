9ef7c9f912ea55c3783331cb37d7a67a6a2acb90
PeterdeGuzman <pdeguz01@Peters-MacBook-Pro-2.local>
2024-11-13 14:11:25 -0500

    update local

--- mylib/extract.py
+++ mylib/extract.py
@@ -29,42 +29,6 @@ def test_encoding_zippedfile(url):
                     print(f"\nEncoding of {filename}: {encoding_info['encoding']}")
 
 
-def check_txtencoding_directory(directory):
-    # Loop through all files in the directory
-    for filename in os.listdir(directory):
-        if filename.endswith(".txt"):
-            file_path = os.path.join(directory, filename)
-
-            # Open the file in binary mode
-            with open(file_path, "rb") as f:
-                raw_data = f.read()
-
-                # Detect encoding
-                result = chardet.detect(raw_data)
-                encoding = result["encoding"]
-                confidence = result["confidence"]
-
-                print(f"File: {filename}")
-                print(f"Detected encoding: {encoding} (Confidence: {confidence:.2f})")
-
-
-def extract(
-    url,
-    filepath,
-    directory,
-):
-    """Extract to file path"""
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    with requests.get(url, timeout=5) as r:
-        with open(filepath, "wb") as f:
-            f.write(r.content)
-    return filepath
-
-
-# add filename argument so you can custom name the downloaded file
-
-
 def extract_zip(url, directory):
     # Creating directory if not present
     if not os.path.exists(directory):
@@ -83,22 +47,3 @@ def extract_zip(url, directory):
     # removing zipped file after extraction
     os.remove(zip_filepath)
     return directory
-
-
-# if __name__ == "__main__":
-#     if os.path.exists("/Users/pdeguz01/Documents/git/PeterdeGuzman_Mini6"):
-#         os.chdir("/Users/pdeguz01/Documents/git/PeterdeGuzman_Mini6")
-#     else:
-#         print("Directory does not exist.")
-# extract(
-#     url="https://s3.amazonaws.com/dl.ncsbe.gov/ENRS/2020_11_03/polling_place_20201103.csv",
-#     filepath="data/pollingplaces_2020.csv",
-#     directory="data",
-# )
-# extract_zip(
-#     url="https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvoter32.zip",
-#     directory="data",
-# )
-# extract_zip(
-#     url="https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvhis32.zip", directory="data"
-# )
